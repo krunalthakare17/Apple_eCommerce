3c24489d103d68e1cdcf0246d6e2c9706e585982
krunalthakare17 <krunalthakare17@gmail.com>
2023-02-27 12:54:01 +0530

    APPLE_ECOMMERSE

---
+++ script.js
@@ -0,0 +1,44 @@
+// slideshow
+const slideShowDiv = () => {
+    for (let i = 1; i <= 6; i++) {
+        const div = document.createElement('div')
+
+
+        div.style.backgroundImage = `url(images/slideshow/section-1-bg-${i}.jpg)`
+
+        i === 1 && div.classList.add("change")
+
+
+        document.querySelector('.slideshow').appendChild(div)
+    }
+}
+slideShowDiv()
+
+
+const divs = document.querySelectorAll('.slideshow div')
+
+let a = 1;
+
+
+const slideshow = () => {
+    setInterval
+    (() => {
+
+        a++
+
+        const div = document.querySelector('.slideshow .change')
+
+        div.classList.remove('change')
+
+    
+
+        if (a < divs.length) {
+            div.nextElementSibling.classList.add('change')
+        }
+        else {
+            a = 1
+        }
+    }, 1000)
+}
+
+slideshow ()
